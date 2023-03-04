2b5e14de6b7a738497955cc59818ed3dbc1bd66c
Ginika Chinonso <ginika.chinonso.francis@gmail.com>
2023-03-04 14:23:18 +0100

    chore: Type setting

--- scripts/deploy.ts
+++ scripts/deploy.ts
@@ -6,14 +6,14 @@ async function main() {
 
   //  Setting up necessary variables
   const [owner] = await ethers.getSigners();
-  const RICHKID = "0x748dE14197922c4Ae258c7939C7739f3ff1db573"
-  const contractName = "MerkleTimi"
-  const contractSymbol = "MTM"
+  const RICHKID: string = "0x748dE14197922c4Ae258c7939C7739f3ff1db573"
+  const contractName: string = "MerkleTimi"
+  const contractSymbol: string = "MTM"
 
 
   // Deploy contract
-  const MERKLETIMI = await ethers.getContractFactory("MerkleTimi")
-  const MerkleTimiContract = await MERKLETIMI.deploy(contractName, contractSymbol);
+  const MERKLETIMI: any = await ethers.getContractFactory("MerkleTimi")
+  const MerkleTimiContract: any = await MERKLETIMI.deploy(contractName, contractSymbol);
 
   await MerkleTimiContract.deployed()
 
@@ -28,11 +28,11 @@ async function main() {
 
 
   // Read file and create tree leaf data
-  const file = fs.readFileSync("./scripts/data/whitelist.csv")
+  const file: any = fs.readFileSync("./scripts/data/whitelist.csv")
 
-  const allLines = file.toString("ascii").split("\n")
+  const allLines: Array<any> = file.toString("ascii").split("\n")
 
-  const res = []
+  const res: Array<any> = []
 
   for (let i = 1; i < allLines.length; i++) {
     res.push(allLines[i].split(", "));
@@ -55,15 +55,15 @@ async function main() {
 
   // Generate proof
   //@ts-ignore
-  const verificationTree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("./scripts/data/tree.json")));
+  const verificationTree: StandardMerkleTree = StandardMerkleTree.load(JSON.parse(fs.readFileSync("./scripts/data/tree.json")));
 
 
-  function getProof(addr: any) {
+  function getProof(addr: string) {
     for (let [i, v] of verificationTree.entries()) {
 
       if (v[0] == addr) {
 
-        const proof = verificationTree.getProof(v)
+        const proof: any = verificationTree.getProof(v)
 
         // console.log(`Address: ${v[0]}`)
         // console.log(`Leaf data: ${v}`)
@@ -74,7 +74,7 @@ async function main() {
     }
   }
 
-  const RichKidProof = await getProof(RICHKID);
+  const RichKidProof: any = await getProof(RICHKID);
 
   console.log(`The Proof of address ${RICHKID} is: ${RichKidProof}`)
 
@@ -85,7 +85,7 @@ async function main() {
 
 
   // Check Rich kid balance before claiming airdrop
-  const BalBefClaim = await MerkleTimiContract.balanceOf(impersonatedSigner.address);
+  const BalBefClaim: number = await MerkleTimiContract.balanceOf(impersonatedSigner.address);
   console.log(`Rich Kid balance before claiming airdrop is: ${BalBefClaim}`);
 
 
@@ -95,7 +95,7 @@ async function main() {
 
 
   // Check Rich kid balance after claiming airdrop
-  const BalAftClaim = await MerkleTimiContract.balanceOf(impersonatedSigner.address);
+  const BalAftClaim: number = await MerkleTimiContract.balanceOf(impersonatedSigner.address);
   console.log(`Rich Kid balance after claiming airdrop is: ${BalAftClaim}`);
 
 } 
